e22c5500ebe21728ee8b7402838df0c3920a93c9
D.Q.R <davidquijanoramos@gmail.com>
2023-10-29 20:03:40 -0500

    Con erroeres
    para actualizar  Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,13 @@ import { FormsModule } from '@angular/forms'; // Añade esta línea
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './components/employee/employee.component';
+import { PersonaComponent } from './components/employee/employee.component';
 import { LoginComponent } from './login/login/login.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmployeeComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule // Añade FormsModule aquí
-  ],
+  declarations: [AppComponent, PersonaComponent, LoginComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,19 +1,20 @@
 import { Persona } from './../../services/persona';
-import { Component, OnInit, Inject,  ViewChild, TemplateRef  } from '@angular/core';
-import { EmployeeService } from '../../services/employee.service';  // Cambia esto a PersonaService si tienes uno
+import { Component, OnInit, Inject, ViewChild, TemplateRef } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
-  selector: 'app-employee',  // Considera cambiar esto a 'app-persona'
+  selector: 'app-persona',
   templateUrl: './employee.component.html',  // Considera cambiar esto a './persona.component.html'
   styleUrls: ['./employee.component.scss']  // Considera cambiar esto a './persona.component.scss'
 })
-export class EmployeeComponent implements OnInit {  // Considera cambiar esto a PersonaComponent
+export class PersonaComponent implements OnInit {
   @ViewChild('editModal') editModal!: TemplateRef<any>;
-  Persona: any[] = [];  // Considera cambiar esto a persona
+  persona: Persona[] = [];
 
   personaAEditar: Persona = {
     id: 0,
@@ -38,18 +39,28 @@ export class EmployeeComponent implements OnInit {  // Considera cambiar esto a 
     edad: 0
   };
 
-  constructor(@Inject(EmployeeService) private employeeService: EmployeeService, public modalService: NgbModal) {  // Cambia esto a PersonaService si tienes uno
-    this.Persona = [];  // Considera cambiar esto a persona
+  constructor(@Inject(EmployeeService) private personaService: EmployeeService, public modalService: NgbModal) {
+    this.personaAEditar = {
+      id: 0,
+      nombre: '',
+      apellido: '',
+      salario: 0,
+      nit: '',
+      correo: '',
+      fechaContrato: new Date(),
+      fechaNacimiento: new Date(),
+      edad: 0
+    };
   }
 
   ngOnInit(): void {
-    this.employeeService.getEmployees().subscribe(data => {  // Cambia esto a getPersonas si tienes uno
-      this.Persona = data;  // Considera cambiar esto a persona
+    this.personaService.getEmployees().subscribe(data => {
+      this.persona = data;
     });
   }
 
   guardarCambios() {
-    this.employeeService.updateEmployee(this.personaAEditar.id, this.personaAEditar).subscribe({
+    this.personaService.updateEmployee(this.personaAEditar.id, this.personaAEditar).subscribe({
       next: response => {
         console.log('Los cambios se han guardado con éxito');
       },
@@ -59,11 +70,15 @@ export class EmployeeComponent implements OnInit {  // Considera cambiar esto a 
     });
   }
 
-  createEmployee() {
-    this.employeeService.createEmployee(this.nuevaPersona).subscribe({
+  createPersona(event: FormEvent) {
+    if (event) {
+      event.preventDefault();
+    }
+
+    this.personaService.createEmployee(this.nuevaPersona).subscribe({
       next: response => {
-        console.log('El empleado se ha creado con éxito');
-        this.Persona.push(this.nuevaPersona);
+        console.log('La persona se ha creado con éxito');
+        this.persona.push(this.nuevaPersona);
         this.nuevaPersona = {
           id: 0,
           nombre: '',
@@ -77,24 +92,30 @@ export class EmployeeComponent implements OnInit {  // Considera cambiar esto a 
         };
       },
       error: error => {
-        console.log('Ha ocurrido un error al crear el empleado', error);
+        console.log('Ha ocurrido un error al crear la persona', error);
       }
     });
   }
 
-  deleteEmployee(id: number) {
-    this.employeeService.deleteEmployee(id).subscribe({
+
+  deletePersona(id: number) {
+    this.personaService.deleteEmployee(id).subscribe({
       next: response => {
-        console.log('El empleado se ha eliminado con éxito');
-        this.Persona = this.Persona.filter(persona => persona.id !== id);
+        console.log('La persona se ha eliminado con éxito');
+        this.persona = this.persona.filter(persona => persona.id !== id);
       },
       error: error => {
-        console.log('Ha ocurrido un error al eliminar el empleado', error);
+        console.log('Ha ocurrido un error al eliminar la persona', error);
       }
     });
   }
 
-  openModal() {
+  openModal(persona: Persona) {
+    if (!persona) {
+      return;
+    }
+
+    this.personaAEditar = persona;
     this.modalService.open(this.editModal);
   }
 
